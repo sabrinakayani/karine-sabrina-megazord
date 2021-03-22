07255dc183b7f508c20c6c13d683df8fc4a80c26
sabrinakayani <70382289+sabrinakayani@users.noreply.github.com>
2021-03-22 12:22:36 -0400

    Update accueil.js

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -5,3 +5,26 @@ var swiperAccueil = new Swiper('.imgPromo .swiper-container', {
     clickable:'true'
   },
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const sectionsList = document.querySelectorAll('.section');
+
+sectionsList.forEach(section =>{
+  const titres = document.querySelectorAll('.titreSection');
+
+  gsap.timeline({
+    scrollTrigger:{
+      markers: true,
+      toggleActions: "play none none reverse",
+      trigger: section,
+    }
+  })
+  .fromTo(titres, {
+    opacity:0,
+  },
+  {
+    opacity:1,
+  })
+});
