7ad0ac14155cb4efa7703568875803f861699424
CorKarine <70382331+RedFaxx@users.noreply.github.com>
2021-04-27 12:26:37 -0400

    Update accueil.js

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -2,19 +2,19 @@ var swiperAccueil = new Swiper('.imgPromo .swiper-container', {
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
-    clickable:'true'
+    clickable: 'true'
   },
 });
 
 gsap.registerPlugin(ScrollTrigger);
 //SCROLL ANIMATION GSAP
-const sectionsList= document.querySelectorAll('.section');
+const sectionsList = document.querySelectorAll('.section');
 
-sectionsList.forEach(section =>{
+sectionsList.forEach(section => {
   const titre = section.querySelectorAll('.titreAccueil');
-  const carte =  section.querySelectorAll('.card');
-  const sectionPromo =  section.querySelector('.swiper-container');
-  const sectionVideo =  section.querySelector('.youtube');
+  const carte = section.querySelectorAll('.card');
+  const sectionPromo = section.querySelector('.swiper-container');
+  const sectionVideo = section.querySelector('.youtube');
 
   gsap.timeline({
     scrollTrigger: {
@@ -24,45 +24,45 @@ sectionsList.forEach(section =>{
       start: 'top 50%',
     }
   })
-  .fromTo(titre, {
-    opacity: 0,
-    fontSize: 0,
-  },
-  {
-    fontSize: 50,
-    opacity: 1,
-    ease: "none",
-  })
+    .fromTo(titre, {
+      opacity: 0,
+      fontSize: 0,
+    },
+      {
+        fontSize: 50,
+        opacity: 1,
+        ease: "none",
+      })
 
-  .fromTo(carte, {
-    opacity: 0,
-    scale: 0.1,
-  },
-  {
-    scale: 1,
-    opacity: 1,
-    ease: "none",
-  })
+    .fromTo(carte, {
+      opacity: 0,
+      scale: 0.1,
+    },
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "none",
+      })
 
-  .fromTo(sectionPromo, {
-    opacity: 0,
-    scale: 0.1,
-  },
-  {
-    scale: 1,
-    opacity: 1,
-    ease: "none",
-  })
+    .fromTo(sectionPromo, {
+      opacity: 0,
+      scale: 0.1,
+    },
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "none",
+      })
 
-  .fromTo(sectionVideo, {
-    opacity: 0,
-    scale: 0.1,
-  },
-  {
-    scale: 1,
-    opacity: 1,
-    ease: "none",
-  })
+    .fromTo(sectionVideo, {
+      opacity: 0,
+      scale: 0.1,
+    },
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "none",
+      })
 });
 
 const cassette = document.querySelector('.spriteCassette');
@@ -75,17 +75,17 @@ gsap.to("main", {
     trigger: 'main',
     onUpdate: (e) => {
       clearTimeout(timeout);
-      timeOut = setTimeout(() =>{
+      timeOut = setTimeout(() => {
         cassette.classList.remove('scrollHaut');
         cassette.classList.remove('scrollBas');
         cassette.classList.add('idle');
-      },500)
-      if (e.direction == 1){
+      }, 500)
+      if (e.direction == 1) {
         cassette.classList.remove('scrollHaut');
         cassette.classList.add('scrollBas');
         cassette.classList.remove('idle');
       }
-      else{
+      else {
         cassette.classList.add('scrollHaut');
         cassette.classList.remove('scrollBas');
         cassette.classList.remove('idle');
